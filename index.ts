3e334da4818fd3c1e707af03a852e75d565d73df
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:48 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@ import reduceWithValueKey from "@unction/reducewithvaluekey";
 import fresh from "@unction/fresh";
 
 export default function dropFirst<A> (count: number) {
-  return function dropFirstCount (orderedList: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string) {
-    return reduceWithValueKey((accumulated: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string) => (value: A) => (index: number): OrderedArray<A> | Set<A> | RecordType<unknown, A> | string => {
+  return function dropFirstCount (orderedList: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
+    return reduceWithValueKey((accumulated: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) => (value: A) => (index: number): OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => {
       if (greaterThan(index)(count - 1)) {
         return append(value)(accumulated);
       }
